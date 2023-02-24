35d6168b8195c42ab4ae10026d0b90594d6fd1f3
santidiazfe <santidiazfe@gmail.com>
2023-02-23 22:09:44 -0300

    Added burguer menu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,60 +1,167 @@
-import React, { useState } from 'react'
-import '../index.css';
-import PicEntrenamiento2 from '../images/Entrenamiento2opacity.jpeg'
+// import React, { useState } from 'react'
+// import '../index.css';
+// import PicEntrenamiento2 from '../images/Entrenamiento2opacity.jpeg'
+// import Info from './Info';
+// import Schedule from './Schedule';
+
+
+
+
+
+// const Main = () => {
+
+// const [funcionalDescriptionVisible, setFuncionalDescriptionVisible] = useState(false);
+// const [deportivoDescriptionVisible, setDeportivoDescriptionVisible] = useState(false);
+
+// const toggleFuncionalDescription = (e) => {
+//   e.preventDefault();
+//   const DescriptionFuncional = document.querySelector('#DescriptionFuncional');
+//   if (DescriptionFuncional.classList.contains('hidden')){
+//     DescriptionFuncional.classList.remove('hidden');
+//     setFuncionalDescriptionVisible(!funcionalDescriptionVisible);
+//   } else {
+//     DescriptionFuncional.classList.add('hidden')
+//     setFuncionalDescriptionVisible(false);
+//   }
+// }
+
+// const toggleDeportivoDescription = (e) => {
+//   e.preventDefault();
+//   const DescriptionDeportivo = document.querySelector('#DescriptionDeportivo');
+//   if (DescriptionDeportivo.classList.contains('hidden')){
+//     DescriptionDeportivo.classList.remove('hidden');
+//     setDeportivoDescriptionVisible(true);
+//   } else {
+//     DescriptionDeportivo.classList.add('hidden')
+//     setDeportivoDescriptionVisible(false)
+//   }
+// }
+
+// const closeDescriptions = () => {
+//   if (funcionalDescriptionVisible) {
+//     const DescriptionDeportivo = document.querySelector('#DescriptionDeportivo');
+//     DescriptionDeportivo.classList.add('hidden');
+//   }
+//   if (deportivoDescriptionVisible) {
+//     const DescriptionFuncional = document.querySelector('#DescriptionFuncional');
+//     DescriptionFuncional.classList.add('hidden')
+//   }
+// }
+
+//   return (
+//     <div className='h-[98%] flex flex-col  bg-center bg-cover   '
+//          style={{backgroundImage:`url("${PicEntrenamiento2}")`}}>
+//         <h1 className='text-[#009e3a] text-3xl font-extrabold mt-32 mb-4 pl-4 '>ENTRENAMIENTO 360</h1>
+        
+//         <h3 className='text-white text-lg w-[85%] pl-4 font-semibold'>Entrenamientos adaptados a tus necesidades y objetivos</h3>
+//         <h4 className='text-white text-lg w-[85%] pl-4 mt-4 text-center font-semibold'>¿Que tipo de entrenamiento estas buscando?</h4>
+//         <div className='flex justify-between mt-4 px-4 ' onClick={closeDescriptions}> 
+//           <button className='bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-black' 
+//                   id='ButtonFuncional' 
+//                   onClick={toggleFuncionalDescription}
+//                   >Funcional 
+//                   {funcionalDescriptionVisible ? <i className="ti ti-chevron-up pl-2"></i> : <i className="ti ti-chevron-down pl-2"></i>}
+//             <p className='hidden bg-[#009e3a] h-74 w-full absolute rounded-lg left-0 mt-6' id='DescriptionFuncional'>Funcional, ipsum dolor sit amet consectetur adipisicing elit. Debitis unde dolore, voluptatibus nesciunt provident accusantium cupiditate, itaque adipisci officia, aut reprehenderit quis expedita sequi totam </p>
+//           </button>
+//           <button className='bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-black' 
+//                   id='ButtonDeportivo' 
+//                   onClick={toggleDeportivoDescription}
+//                   >Deportivo 
+//                    {deportivoDescriptionVisible ? <i className="ti ti-chevron-up pl-2"></i> : <i className="ti ti-chevron-down pl-2"></i>}
+//           <p className='hidden bg-[#009e3a] h-78 w-full absolute rounded-lg left-0 mt-6 ' id='DescriptionDeportivo'> Deportivo, ipsum dolor sit amet consectetur adipisicing elit. Debitis unde dolore, voluptatibus nesciunt provident accusantium cupiditate, itaque adipisci officia, aut reprehenderit quis expedita sequi totam </p>
+//           </button>
+//         </div>
+//         <div className='h-48 w-full bg-gradient-to-t from-black to-transparent '></div>
+//         <Schedule/>
+//         <Info/>
+        
+//     </div>
+//   )
+// }
+
+// export default Main
+
+import React, { useState } from 'react';
+import PicEntrenamiento2 from '../images/Entrenamiento2opacity.jpeg';
 import Info from './Info';
 import Schedule from './Schedule';
 
-
-const clickFuncional = (e) => {
-  e.preventDefault();
-  const DescriptionFuncional = document.querySelector('#DescriptionFuncional');
-  if (DescriptionFuncional.classList.contains('hidden')){
-    DescriptionFuncional.classList.remove('hidden')
-  } else {
-    DescriptionFuncional.classList.add('hidden')
-  }
-}
-
-const clickDeportivo = (e) => {
-  e.preventDefault();
-  const DescriptionDeportivo = document.querySelector('#DescriptionDeportivo');
-  if (DescriptionDeportivo.classList.contains('hidden')){
-    DescriptionDeportivo.classList.remove('hidden')
-  } else {
-    DescriptionDeportivo.classList.add('hidden')
-  }
-}
-
-
-
 const Main = () => {
+  const [openFuncional, setOpenFuncional] = useState(false);
+  const [openDeportivo, setOpenDeportivo] = useState(false);
+
+  const toggleFuncional = () => {
+    setOpenFuncional((prevState) => !prevState);
+    setOpenDeportivo(false);
+  };
+
+  const toggleDeportivo = () => {
+    setOpenDeportivo((prevState) => !prevState);
+    setOpenFuncional(false);
+  };
+
   return (
-    <div className='h-[98%] flex flex-col  bg-center bg-cover   '
-    style={{backgroundImage:`url("${PicEntrenamiento2}")`}}>
-        <h1 className='text-[#009e3a] text-3xl font-extrabold mt-32 mb-4 pl-4 '>ENTRENAMIENTO 360</h1>
+    <div
+      className="h-[98%] flex flex-col bg-center bg-cover"
+      style={{ backgroundImage: `url("${PicEntrenamiento2}")` }}
+    >
+      <h1 className="text-[#009e3a] text-3xl font-extrabold mt-32 mb-4 pl-4 ">
+        ENTRENAMIENTO 360
+      </h1>
+
+      <h3 className="text-white text-lg w-[85%] pl-4 font-semibold">
+        Entrenamientos adaptados a tus necesidades y objetivos
+      </h3>
+      <h4 className="text-white text-lg w-[85%] pl-4 mt-4 text-center font-semibold">
+        ¿Que tipo de entrenamiento estas buscando?
+      </h4>
+      <div className="flex justify-between mt-4 px-4 ">
+        <button
+          className="bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-zinc-800"
+          id="ButtonFuncional"
+          onClick={toggleFuncional}
+        >
+          Funcional{' '}
+          {openFuncional ? (
+            <i className="ti ti-chevron-up pl-2"></i>
+          ) : (
+            <i className="ti ti-chevron-down pl-2"></i>
+          )}
+          {openFuncional && (
+            <p className="bg-[#009e3a] h-74 w-full absolute rounded-lg left-0 mt-6">
+              Funcional, ipsum dolor sit amet consectetur adipisicing elit.
+              Debitis unde dolore, voluptatibus nesciunt provident accusantium
+              cupiditate, itaque adipisci officia, aut reprehenderit quis
+              expedita sequi totam
+            </p>
+          )}
+        </button>
+        <button
+          className="bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-zinc-800"
+          id="ButtonDeportivo"
+          onClick={toggleDeportivo}
+        >
+          Deportivo{' '}
+          {openDeportivo ? (
+            <i className="ti ti-chevron-up pl-2"></i>
+          ) : (
+            <i className="ti ti-chevron-down pl-2"></i>
+          )}
+          {openDeportivo && (
+            <p className="bg-[#009e3a] h-78 w-full absolute rounded-lg left-0 mt-6">
+              Deportivo, ipsum dolor sit amet consectetur adipisicing elit.
+              Debitis unde dolore, voluptatibus nesciunt provident accusantium
+              cupiditate, itaque adipisci officia, aut reprehenderit quis
+              expedita sequi totam
+            </p>
+          )}
+        </button>
+      </div>        
+      <div className='h-48 w-full bg-gradient-to-t from-black to-transparent '></div>
+      <Schedule/>
+      <Info/>
         
-        <h3 className='text-white text-lg w-[85%] pl-4 font-semibold'>Entrenamientos adaptados a tus necesidades y objetivos</h3>
-        <h4 className='text-white text-lg w-[85%] pl-4 mt-4 text-center font-semibold'>¿Que tipo de entrenamiento estas buscando?</h4>
-        <div className='flex justify-between mt-4 px-4 '> 
-          <button className='bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-black' 
-                  id='ButtonFuncional' 
-                  onClick={clickFuncional} >Funcional 
-                  <i className="ti ti-chevron-down pl-2"></i>
-            <p className='hidden bg-[#009e3a] h-74 w-full absolute rounded-lg left-0 mt-6' id='DescriptionFuncional'>Funcional, ipsum dolor sit amet consectetur adipisicing elit. Debitis unde dolore, voluptatibus nesciunt provident accusantium cupiditate, itaque adipisci officia, aut reprehenderit quis expedita sequi totam </p>
-          </button>
-          <button className='bg-[#009e3a] h-12 w-40 text-white text-xl rounded-lg font-semibold shadow-md shadow-black' 
-                  id='ButtonDeportivo' 
-                  onClick={clickDeportivo} >Deportivo 
-                  <i className="ti ti-chevron-down pl-2"></i>
-          <p className='hidden bg-[#009e3a] h-78 w-full absolute rounded-lg left-0 mt-6 ' id='DescriptionDeportivo'> Deportivo, ipsum dolor sit amet consectetur adipisicing elit. Debitis unde dolore, voluptatibus nesciunt provident accusantium cupiditate, itaque adipisci officia, aut reprehenderit quis expedita sequi totam </p>
-          </button>
-        </div>
-        <div className='h-48 w-full bg-gradient-to-t from-black to-transparent '></div>
-        <Schedule/>
-        <Info/>
-        
-    </div>
-  )
+    </div>   )
 }
 
 export default Main
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,86 @@
-import React from 'react'
-import Logo from '../images/BlackLogo.jpeg'
+// import React from 'react';
+// import Logo from '../images/Logo.jpg';
 
+// const Nav = () => {
+//   return (
+//     <nav className='flex items-center justify-between fixed top-0 w-full px-4 py-2 bg-black text-white z-10'>
+//       <div className='flex items-center'>
+//         <img className='h-20' src={Logo} alt='Logo' />
+//       </div>
+//       <button className='text-3xl hover:text-gray-400 focus:outline-none'>
+//         <i className='ti ti-menu-2'></i>
+//       </button>
+//     </nav>
+//   );
+// };
+
+// export default Nav;
+
+import React, { useState } from 'react';
+import Logo from '../images/Logo.jpg';
 
 const Nav = () => {
-  return (
-    <nav className='text-2xl bg-[#000000] w-full h-24 flex justify-between fixed'>
-        <img src={Logo}  alt="" />
-        <i className="ti ti-dots-vertical text-white mt-8"></i>
-    </nav>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Nav
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+  return (
+    <nav className='relative flex items-center justify-between w-full h-24 px-4 py-2 bg-black text-white'>
+      <div className='flex items-center'>
+        <img className='h-20' src={Logo} alt='Logo' />
+      </div>
+      <button
+        className='text-3xl hover:text-gray-400 focus:outline-none lg:hidden'
+        onClick={handleToggle}
+      >
+        {isOpen ? (
+          <div
+            className='text-white hover:text-gray-400 text-3xl focus:outline-none absolute top-0 right-0 mt-8 mr-4'
+            
+            onClick={handleToggle}
+          >
+            <i className='ti ti-x'></i>
+          </div>
+        ) : (
+          <i className='ti ti-menu'></i>
+        )}
+      </button>
+      <ul
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } absolute top-24 left-0 w-full bg-black bg-opacity-75 z-20 flex flex-col justify-center items-center text-4xl`}
+      >
+        <li className='py-4'>
+          <a href='#' className='text-white hover:text-gray-400'>
+            Home
+          </a>
+        </li>
+        <li className='py-4'>
+          <a href='#' className='text-white hover:text-gray-400'>
+            About Us
+          </a>
+        </li>
+        <li className='py-4'>
+          <a href='#' className='text-white hover:text-gray-400'>
+            Services
+          </a>
+        </li>
+        <li className='py-4'>
+          <a href='#' className='text-white hover:text-gray-400'>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
+
+
+
+
+
