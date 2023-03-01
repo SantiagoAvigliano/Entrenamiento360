e5e5b6dec5b290c2339bc07446a4a6b3f520db06
santidiazfe <santidiazfe@gmail.com>
2023-03-01 17:13:48 -0300

    changes in Booking page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,32 +1,3 @@
-// import React from 'react'
-// import Nav from '../components/Nav'
-// const Booking = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <form action='' className='bg-[#21683c] flex flex-col h-screen text-white justify-start pt-48 px-4'>
-//         <label className='text-2xl font-bold'>Seleccioná el horario:</label> 
-//         <select className='h-10 tet-xl text-black rounded-md bg-white font-semibold my-6 pl-4'>
-//           <option value="8">8:00</option>
-//           <option value="9">9:00</option>
-//           <option value="10">10:00</option>
-//           <option value="11">11:00</option>
-//           <option value="16">16:00</option>
-//           <option value="17">17:00</option>
-//           <option value="18">18:00</option>
-//           <option value="19">19:00</option>
-//           <option value="20">20:00</option>
-//         </select>
-//         <div className='flex justify-center items-center py-4'>
-//           <button className='bg-[#0d3b1e] w-32 font-bold hover:text-[#009e3a] hover:bg-[#104423]  rounded-md px-5 py-[5px] shadow-md hover:shadow-4xl'>Enviar</button>
-//         </div>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default Booking
-
 import React, { useState } from 'react';
 import Nav from '../components/Nav';
 
@@ -35,22 +6,18 @@ const Booking = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Get the current time and the selected hour
+  
     const now = new Date();
     const selectedHour = Number(selectedTime.split(':')[0]);
-
-    // Check if the selected hour is within the allowed range
+  
     if (now.getHours() < selectedHour ||
-        (now.getHours() === selectedHour && now.getMinutes() < 45)) {
-      // The selected hour is valid, submit the form
-      console.log('Submitting form...');
-      // Replace this with your own code to submit the form
+        (now.getHours() === selectedHour && now.getMinutes() < -15)) {
+      console.log('Enviando horario...');
     } else {
-      // The selected hour is not valid, display an error message
       alert('El horario seleccionado no está disponible.');
     }
   };
+  
 
   return (
     <div>
@@ -73,6 +40,9 @@ const Booking = () => {
           <option value="19:00">19:00</option>
           <option value="20:00">20:00</option>
         </select>
+        <div>
+          <p>{selectedTime}</p>
+        </div>
         <div className='flex justify-center items-center py-4'>
           <button
             type='submit'
